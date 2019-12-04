38c1b14cd615364602d30e635ebe61c2a8aaa488
André Andersen <andre.andersen@sogeti.com>
2019-12-04 21:25:56 +0100

    Day 4, Part 1

--- src/AdventOfCode.Solutions/Days/Day04/Day04.cs
+++ src/AdventOfCode.Solutions/Days/Day04/Day04.cs
@@ -1,7 +1,7 @@
 ﻿using AdventOfCode.Solutions.BuildingBlocks;
-
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Days.Day04
 {
@@ -17,8 +17,43 @@ namespace AdventOfCode.Solutions.Days.Day04
                 .Select(p => byte.Parse(p.ToString()))
                 .ToArray();
 
+            var validPasswords = 0;
 
-            return 0;
+            for (int i = min; i < max; i++)
+            {
+                if (Validate(i))
+                    validPasswords++;
+            }
+
+            return validPasswords;
+        }
+
+        [MethodImpl(
+            MethodImplOptions.AggressiveInlining |
+            MethodImplOptions.AggressiveOptimization)]
+        public static bool Validate(int password)
+        {
+            Span<byte> sequence = new byte[1 + (int)Math.Log10(password)];
+            for (int i = sequence.Length - 1; i >= 0; i--)
+            {
+                password = Math.DivRem(password, 10, out var d);
+                sequence[i] = (byte)d;
+            }
+
+            if (sequence.Length != 6)
+                return false;
+
+            bool hasDouble = false;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (sequence[i] < sequence[i - 1])
+                    return false;
+
+                if (sequence[i - 1] == sequence[i])
+                    hasDouble = true;
+            }
+
+            return hasDouble;
         }
 
         public object Second(string input) => 0;
---
+++ src/AdventOfCode.Solutions/Days/Day04/Tests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Solutions.Days.Day04
+{
+    [Trait("Day", "04")]
+    [Collection("Day 04")]
+    public class Day04_Tests
+    {
+        [Theory]
+        [InlineData(223450)]
+        [InlineData(123789)]
+        [InlineData(12345)]
+        public void Invalid_Password_Returns_False(int password)
+        {
+            Day04.Validate(password).Should().BeFalse();
+        }
+
+        [Theory(DisplayName = "Valid Password Returns True")]
+        [InlineData(122345)]
+        [InlineData(111111)]
+        public void Valid_Password_Returns_True(int password)
+        {
+            Day04.Validate(password).Should().BeTrue();
+        }
+    }
+}
