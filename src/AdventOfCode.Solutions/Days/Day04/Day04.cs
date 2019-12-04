e70e737ad79f1fe1a545fd284901d8be39ac5c76
André Andersen <andre.andersen@sogeti.com>
2019-12-04 16:02:28 +0100

    Setting up Day 4

---
+++ src/AdventOfCode.Solutions/Days/Day04/Day04.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Solutions.BuildingBlocks;
+
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Days.Day04
+{
+    public class Day04 : IDay
+    {
+        public object First(string input)
+        {
+            var split = input.Split('-'); ;
+            var min = int.Parse(split[0]);
+            var max = int.Parse(split[1]);
+
+            ReadOnlySpan<byte> seq = split[0]
+                .Select(p => byte.Parse(p.ToString()))
+                .ToArray();
+
+
+            return 0;
+        }
+
+        public object Second(string input) => 0;
+    }
+}
